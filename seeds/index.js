d530bec36d5b77e09aafd8ce7a41f84f975d1903
Kalpesh3881 <147605705+Kalpesh3881@users.noreply.github.com>
2024-03-31 15:12:27 +0200

    Create seedDB function to populate AdventureNest collection with sample data

---
+++ seeds/index.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
+const Nest = require("../models/AdventureNest");
+
+//Database connection
+mongoose.connect("mongodb://localhost:27017/AdventureNest");
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection error:"));
+db.once;
+"open",
+  () => {
+    console.log("Database Connected");
+  };
+
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+  await Nest.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const nest = new Nest({
+      location: `${cities[random1000].city},${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+    });
+
+    await nest.save();
+  }
+};
+
+seedDB().then(() => {
+  mongoose.connection.close();
+});
